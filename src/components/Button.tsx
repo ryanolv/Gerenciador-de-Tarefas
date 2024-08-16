9d402d3cd37fcc8facbd6159712be54640bde012
Ryan Oliveira <ryanprofissional02@gmail.com>
2024-08-16 15:33:48 -0300

    feat: add size and secundary variant to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,20 +2,35 @@ import React from "react";
 
 interface ButtonProps {
   children: React.ReactNode;
-  variant?: "primary" | "ghost";
+  variant?: "primary" | "secundary" | "ghost";
+  size?: "small" | "large";
+  className?: any;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-function Button({ children, variant = "primary", onClick }: ButtonProps) {
+function Button({
+  children,
+  variant = "primary",
+  size = "small",
+  className,
+  onClick,
+}: ButtonProps) {
   const getVariantClasses = () => {
     if (variant === "primary") return "bg-[#00ADB5] text-white";
 
     if (variant === "ghost") return "bg-transparent text-[#818181]";
+
+    if (variant === "secundary") return "bg-[#EEEEEE] text-[#35383E]";
+  };
+
+  const getSizeClasses = () => {
+    if (size === "small") return "py-1 text-xs";
+    if (size === "large") return "py-2 text-sm";
   };
 
   return (
     <button
-      className={`flex items-center gap-2 rounded-md px-3 py-1 text-xs transition hover:opacity-90 ${getVariantClasses()}`}
+      className={`flex items-center justify-center gap-2 rounded-md px-3 transition hover:opacity-90 ${getVariantClasses()} ${getSizeClasses()} ${className}`}
       onClick={onClick}
     >
       {children}
