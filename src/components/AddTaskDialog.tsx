7a288bd0992085060168d0491623d5a4a41181a9
Ryan Oliveira <ryanprofissional02@gmail.com>
2024-09-04 16:59:25 -0300

    feat: add validation errors to AddTaskDialog component

--- src/components/AddTaskDialog.tsx
+++ src/components/AddTaskDialog.tsx
@@ -17,6 +17,11 @@ interface AddTaskDialogProps {
   handleSubmit: (task: TasksProps) => void;
 }
 
+interface ErrorProps {
+  inputName: string;
+  message: string;
+}
+
 function AddTaskDialog({
   isOpen,
   handleClose,
@@ -25,6 +30,7 @@ function AddTaskDialog({
   const [title, setTitle] = useState<any>();
   const [time, setTime] = useState<any>("morning");
   const [description, setDescription] = useState<any>();
+  const [errors, setErrors] = useState<ErrorProps[]>([]);
 
   useEffect(() => {
     if (!isOpen) {
@@ -38,8 +44,32 @@ function AddTaskDialog({
   if (!isOpen) return null;
 
   const handleSaveClick = () => {
-    if (!title.trim() || !description.trim()) {
-      return alert("Preencha todos os campos.");
+    const newErrors: ErrorProps[] = [];
+
+    if (!title.trim()) {
+      newErrors.push({
+        inputName: "title",
+        message: "O título é obrigatório",
+      });
+    }
+
+    if (!time.trim()) {
+      newErrors.push({
+        inputName: "time",
+        message: "O horário é obrigatório",
+      });
+    }
+
+    if (!description.trim()) {
+      newErrors.push({
+        inputName: "description",
+        message: "A descrição é obrigatório",
+      });
+    }
+
+    setErrors(newErrors);
+    if (newErrors.length > 0) {
+      return;
     }
 
     handleSubmit({
@@ -51,6 +81,16 @@ function AddTaskDialog({
     });
     handleClose();
   };
+
+  const titleError = errors.find(
+    (error: ErrorProps) => error.inputName === "title"
+  );
+  const timeError = errors.find(
+    (error: ErrorProps) => error.inputName === "time"
+  );
+  const descriptionError = errors.find(
+    (error: ErrorProps) => error.inputName === "description"
+  );
 
   return (
     <CSSTransition
@@ -81,12 +121,19 @@ function AddTaskDialog({
                   placeholder="Insira o título da tarefa"
                   value={title}
                   onChange={(e) => setTitle(e.target.value)}
+                  errorMessage={titleError?.message}
                 />
 
                 <TimeSelect
                   value={time}
                   onChange={(e: any) => setTime(e.target.value)}
+                  errorMessage={timeError?.message}
                 />
+                {timeError && (
+                  <p className="text-left text-xs text-red-500">
+                    {timeError.message}
+                  </p>
+                )}
 
                 <Input
                   id="description"
@@ -94,7 +141,9 @@ function AddTaskDialog({
                   placeholder="Descreva a tarefa"
                   value={description}
                   onChange={(e) => setDescription(e.target.value)}
+                  errorMessage={descriptionError?.message}
                 />
+
                 <div className="flex gap-3">
                   <Button
                     variant="secundary"
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,9 +2,10 @@ import InputLabel from "./InputLabel";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
+  errorMessage?: string;
 }
 
-function Input({ label, ...rest }: InputProps) {
+function Input({ label, errorMessage, ...rest }: InputProps) {
   return (
     <div className="flex flex-col space-y-1 text-left">
       <InputLabel htmlFor={rest.id}>{label}</InputLabel>
@@ -12,6 +13,7 @@ function Input({ label, ...rest }: InputProps) {
         className="rounded-lg border border-solid border-[#ECECEC] px-4 py-3 outline-[#00ADB5] placeholder:text-sm placeholder:text-[9A9C9F]"
         {...rest}
       />
+      <p className="text-left text-xs text-red-500">{errorMessage}</p>
     </div>
   );
 }
