32f17b072e5c84259095c9f1e608654efe1209ea
Ryan Oliveira <ryanprofissional02@gmail.com>
2024-09-05 09:33:43 -0300

    refactor: add tailwind-variants to SideBarButton component

--- src/components/SidebarButton.tsx
+++ src/components/SidebarButton.tsx
@@ -1,26 +1,24 @@
 import React from "react";
+import { tv } from "tailwind-variants";
 
 interface SidebarButtonProps {
   children: React.ReactNode;
-  variant?: "unselected" | "selected";
+  color?: "unselected" | "selected";
 }
 
-function SidebarButton({ children, variant }: SidebarButtonProps) {
-  const getVariantClasses = () => {
-    if (variant === "unselected") {
-      return "text-brand-dark-blue";
-    }
-
-    if (variant === "selected") {
-      return "bg-brand-primary bg-opacity-15 text-brand-primary";
-    }
-  };
+function SidebarButton({ children, color }: SidebarButtonProps) {
+  const sidebar = tv({
+    base: "flex items-center gap-2 rounded-lg px-6 py-3",
+    variants: {
+      color: {
+        unselected: "text-brand-dark-blue",
+        selected: "bg-brand-primary bg-opacity-15 text-brand-primary",
+      },
+    },
+  });
 
   return (
-    <a
-      href=""
-      className={`flex items-center gap-2 rounded-lg px-6 py-3 ${getVariantClasses()}`}
-    >
+    <a href="" className={sidebar({ color })}>
       {children}
     </a>
   );
